a4d6d4d360b02eae903d4a09f1bc3592e519af3f
ThatNerd404 <bcotterman06@gmail.com>
2025-07-31 17:40:41 -0500

    Adding more commands and messing with formatting also adding logs

--- Interface.py
+++ Interface.py
@@ -1,8 +1,10 @@
 from da_console import console
 from LLAMA_Worker import Llama_Worker
+from logging.handlers import RotatingFileHandler
 import os
-import sys
-import time
+import logging
+import sys 
+
 class Main_Interface():
     def __init__(self):
                 os.system('clear')
@@ -21,21 +23,57 @@ class Main_Interface():
         """, justify="center", style="#fcc200")
                 console.rule("", style="#fcc200")
                 self.convo_history = [{"role": "system", "content": "You are a helpful AI assisant named APOLLO. You refer to the user as Sir Cotterman.\n                          "}]
+                
+                # Setup logger and rotating file handler
+                self.logger = logging.getLogger("logger")
+                self.logger.setLevel(logging.DEBUG)
+                handler = RotatingFileHandler(os.path.join(
+                'Logs', 'log.log'), maxBytes=100000, backupCount=5, encoding="utf-8")
+                formatter = logging.Formatter(
+                '%(asctime)s - %(levelname)s - %(message)s',"%Y-%m-%d %H:%M:%S")
+                handler.setFormatter(formatter)
+                self.logger.addHandler(handler)
+
+                self.logger.debug("Finished Initialization")
+              
     def run(self):
         console.print("Input your prompt then press enter. Type /quit to leave.")
         while True:
-               query = input("\nUser: ")
-               if query == "/quit":
-                      console.print("bye bye!",style="green")
-                      sys.exit(1)
-               else:
-                     self.convo_history.append({"role":"user","content": query})
-                     Llama = Llama_Worker(model_path="/home/smartfella/programming_junk/CLI_APOLLO/Models/capybarahermes-2.5-mistral-7b.Q2_K.gguf",
-                                          messages=self.convo_history,
-                                          threads=8,
-                                          context=2048,
-                                          gpu_layers=0
-                                          )
-                     with console.status("Generating Response...", spinner="dots") as status:
-                            response = Llama.generate_response(status)
-                     self.convo_history.append({"role":"assistant","content": response})
+               try:
+                     query = input("\nUser: ")
+                     if query == "/quit":
+                            self.logger.info("quit command has been used")
+                            
+                            while True:
+                                confirm = input("\nAre you sure you would like to quit? Y or N?\nUser: ")
+                                if confirm.upper() == "Y":
+                                   console.print("Bye Bye!")
+                                   sys.exit(1)
+                                elif confirm.upper() == "N":
+                                   break
+                                
+                                else:
+                                   console.print("\nInvalid Input! Try Y or N!")
+                               
+                     elif query == "/help":
+                            console.print("Figure it out bonehead!")
+                     
+                     elif query == "reset":
+                           self.logger.info("reset command has been used")
+                           self.convo_history = [{"role": "system", "content": "You are a helpful AI assisant named APOLLO. You refer to the user as Sir Cotterman.\n                          "}]
+                     
+                     else:
+                            self.logger.info("Response generation has begun")
+                            self.convo_history.append({"role":"user","content": query})
+                            Llama = Llama_Worker(model_path=os.path.join("Models","capybarahermes-2.5-mistral-7b.Q2_K.gguf"),
+                                                 messages=self.convo_history,
+                                                 threads=8,
+                                                 context=2048,
+                                                 gpu_layers=0
+                                                 )
+                            with console.status("Generating Response...", spinner="dots") as status:
+                                   response = Llama.generate_response(status)
+                            self.convo_history.append({"role":"assistant","content": response})
+               except Exception as e:
+                      self.logger.error(f"Error Occurred: {e}")
+                      console.print(f"Apollo has run into an unexpected error: {e}",style="red bold")
