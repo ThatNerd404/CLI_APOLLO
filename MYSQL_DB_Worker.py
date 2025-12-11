44c155851046cecaddce9164ea42b95da9219626
ThatNerd404 <bcotterman06@gmail.com>
2025-12-10 21:45:25 -0600

    further messing around with mysql and getting my db setup

--- MYSQL_DB_Worker.py
+++ MYSQL_DB_Worker.py
@@ -7,8 +7,10 @@ class MySql_Worker():
             host = "100.111.62.92",
             user = "root",
             password = "Secret_PW",
-            port=8000
+            port=8000,
+            database="Conversation_Storage"
 )
-        print(self.mydb)
+        self.cursor = self.mydb.cursor()
+        self.mydb.close()
 if __name__ == "__main__":
     msw = MySql_Worker()
