8d71a7963a8454b254c543b896fd8cf7ebd5aa9d
ThatNerd404 <bcotterman06@gmail.com>
2025-12-11 19:45:00 -0600

    adding a lil error handling plus testing out the db

--- MYSQL_DB_Worker.py
+++ MYSQL_DB_Worker.py
@@ -1,5 +1,9 @@
 import json
 import mysql.connector
+from mysql.connector import errorcode
+
+# TODO: create table with conversation id or name, the conversation, the timestamp of when it was saved and other metadata like length, model used,etc
+# TODO: add basic error handling to worker
 
 class MySql_Worker():
     def __init__(self):
@@ -11,6 +15,14 @@ class MySql_Worker():
             database="Conversation_Storage"
 )
         self.cursor = self.mydb.cursor()
+        try:
+            self.cursor.execute("SHOW DATABASES")
+            result = self.cursor.fetchall()
+            print(result)
+
+        except mysql.connector.Error as e:
+            print(f"bro we ran into an error {e}\n Error number: {e.errno}")
+
         self.mydb.close()
 if __name__ == "__main__":
     msw = MySql_Worker()
